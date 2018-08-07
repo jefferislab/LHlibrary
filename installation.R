cf3b47fdd338bb608f539d9c7f1d0f0a361a0666
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-08-07 19:56:13 +0100

    trying to update other packages

--- installation.R
+++ installation.R
@@ -17,6 +17,6 @@ library(devtools)
 github.packages = c("karthik/wesanderson","jefferislab/elmr","jefferislab/lhns","jefferislab/lhlite","alexanderbates/catnat","jefferis/flycircuit","jefferis/vfbr",'hadley/ggplot2')
 for(gp in github.packages){
   if (!require(gp,character.only = TRUE)){
-    devtools::install_github(gp, dependencies=TRUE, lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.4/")
+    devtools::install_github(gp, dependencies=TRUE, auth_token = "17c7e1b411363b4e2f84bd2c970a29f229809366",lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.4/")
   } 
 }
