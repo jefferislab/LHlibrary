fb41c9af0ceb0467c7f355064148b1f03a885727
alexanderbates <alexander.shakeel.bates@gmail.com>
2018-08-07 19:54:09 +0100

    Attempting to update lhlite

--- updateData.R
+++ updateData.R
@@ -5,5 +5,5 @@
 
 # Not from CRAN
 library(devtools)
-devtools::install_github(repo="lhlite", username = "jefferislab", dependencies=TRUE, host = "https://api.github.com", lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.4/")
+devtools::install_github(repo="jefferislab/lhlite", dependencies=TRUE, auth_token = "17c7e1b411363b4e2f84bd2c970a29f229809366", host = "https://api.github.com", lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.4/")
 
