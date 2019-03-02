c7fb2c2e982a963cd6cc65afc375f3b2cb1d2b18
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-03-02 15:57:24 +0000

    Change library location for package updates

--- installation.R
+++ installation.R
@@ -3,20 +3,20 @@
 ########################
 
 # From CRAN
-library(devtools)
-cran.packages = c("shinyWidgets","devtools","shiny","shinyBS", "shinyURL","shinythemes","slickR","plotly","colourpicker","heatmaply","data.table",
+options(Ncpus = 4)
+cran.packages = c("devtools","rgl","shinyWidgets","devtools","shiny","shinyBS", "shinyURL","shinythemes","slickR","plotly","colourpicker","heatmaply","data.table",
                   "ggplot2","DT","shinydashboard")
 for(cp in cran.packages){
   if (!require(cp,character.only = TRUE)){
-    utils::install.packages(cp, repos='http://cran.us.r-project.org', dependencies=TRUE)
+    utils::install.packages(cp, repos='http://cran.us.r-project.org', dependencies=TRUE, lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.5/")
   } 
 }
 
 # Not from CRAN
 library(devtools)
-github.packages = c("karthik/wesanderson","jefferislab/elmr","jefferislab/lhns","jefferislab/lhlite","alexanderbates/catnat","jefferis/flycircuit","jefferis/vfbr",'hadley/ggplot2')
+github.packages = c("jefferislab/nat.templatebrains","jefferislab/lhlite","karthik/wesanderson","jefferislab/elmr","jefferis/nat.utils","jefferislab/lhns","jefferislab/lhlite","alexanderbates/catnat","jefferis/flycircuit","jefferis/vfbr",'hadley/ggplot2',"aoles/shinyURL")
 for(gp in github.packages){
-    devtools::install_github(gp, dependencies=TRUE)
+    devtools::install_github(gp, dependencies=TRUE, lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.5/")
 }
 
 q()
