fac9a8ee15de88c048206c58f117f8b0ab6f700a
alexanderbates <alexander.shakeel.bates@gmail.com>
2019-03-02 19:57:59 +0000

    Removed redundant lines from updates to updateData.R

--- updateData.R
+++ updateData.R
@@ -7,6 +7,5 @@
 options(Ncpus = 4)
 library(devtools)
 Sys.setenv(RGL_USE_NULL=TRUE)
-devtools::install_github(repo="jefferislab/lhlite", dependencies=TRUE, host = "https://api.github.com", lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.5/", auth_token = "bb13ad495c8a15b2f77ebc7362e1fa7b742d749c")
-devtools::install_github(repo="jefferislab/LHlibrary", dependencies=TRUE, host = "https://api.github.com", lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.5/", auth_token = "bb13ad495c8a15b2f77ebc7362e1fa7b742d749c")
+devtools::install_github(repo="jefferislab/lhlite", dependencies=TRUE, host = "https://api.github.com", lib = "/home/shiny/R/x86_64-pc-linux-gnu-library/3.5/")
 q()
